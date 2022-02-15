54eb8fb007fea6492584c8394f44a1ff6fe21ba6
Crouwing <mr.crouwing@gmail.com>
2022-02-15 18:45:45 +0300

    6. Классификация типов данных в JS

--- JS/script_one.js
+++ JS/script_one.js
@@ -1,17 +1,29 @@
 "use strict";
 
-a = 15;
-console.log(a);
+// let number = 4.6;
 
-let number = 5; 
-const leftBorderWidth = 1;
+// console.log(4/0);
+// console.log('string' * 9);
 
-number = 10;
-console.log(number);
+// const persone = 'Alex';
+
+// const bool = true;
+
+// console.log(something);
+
+// let und;
+// console.log(und);
 
 const obj = {
-    a:50
+    name: "John",
+    age: 25,
+    isMarried: false
 };
 
-obj.a = 10;
-console.log(obj);
+console.log(obj.name);
+
+let arr = [
+    'plum.png', 'orange.jpg', 6, 'apple.bmp'
+];
+
+console.log(arr[1]);
