64ec43d96cfa3b45e70b51c2503dd4b793a3ff48
Crouwing <mr.crouwing@gmail.com>
2022-02-21 16:55:27 +0300

    9. Операторы в JS

--- JS/script_one.js
+++ JS/script_one.js
@@ -1,9 +1,22 @@
 "use strict";
 
-const category = 'toys';
-console.log(`https://somuerl.com/${category}`);
+console.log('arr' + ' - object');
+console.log(4 + +'5');
 
-const user = 'Ivan';
+let incr = 10,
+    decr = 10;
 
-alert(`Привет, ${user}`);
+// ++incr;
+// --decr;
 
+console.log(++incr);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log(4*2 == 8);
+
+const IsChecked = true,
+        IsClose = false; 
+        
+console.log(IsChecked || IsClose);
