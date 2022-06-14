8038438a267787202a1e3372b7fab0bdccccf7b2
Crouwing <mr.crouwing@gmail.com>
2022-06-14 18:09:24 +0300

    25. Функции, стрелочные ф-ции (ES6)

--- JS/script_one.js
+++ JS/script_one.js
@@ -1,21 +1,35 @@
 "use strict";
-let num = 50;
 
-// while (num < 55) {
-//     console.log(num);
-//     num++;
-// }
+let num = 20;
 
-// do {
-//     console.log(num);
-//     num++; 
-// }
-// while (num < 55);
-
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
-    }
-    console.log(i);
+function showFirstMessage (text) {
+    console.log (text);
+    let num = 30;
+    console.log(num);
 }
+
+showFirstMessage("Hellow, World!");
+console.log(num);
+
+function calc (a, b) {
+    return (a + b);
+}
+
+console.log (calc(3, 5));
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+
+console.log(anotherNum);
+
+const logger = function() {
+    console.log("Hellow!");
+};
+
+logger();
+
+const calc = (a, b) => a + b;
