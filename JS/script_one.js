eb306e3314c8947055f5e413a905adc1f1100984
Crouwing <mr.crouwing@gmail.com>
2022-06-17 12:24:29 +0300

    27. (д) Про важность return

--- JS/script_one.js
+++ JS/script_one.js
@@ -2,9 +2,35 @@
 
 const usdCurr = 28;
 const uerCurr = 30;
+const discount = 0.9;
 
 function convert (amount, curr) {
-console.log(curr * amount);
+    return curr * amount;
 }
 
-convert (10, uerCurr);
+function promo (result) {
+    console.log(result * discount);
+}
+
+const res = convert(500, usdCurr);
+promo(res);
+
+// promo(convert(500, usdCurr)); - Альтернатива. Функция в функции.
+
+function test () {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+        if (i === 3) {
+            return;
+        }
+    }
+        console.log("done");
+}
+
+test ();
+
+function doNothing () {}
+
+console.log(doNothing() === undefined);
+
+// Любая фукнция всегда возвращает что то, даже если не прирывать возвращает undefined )
