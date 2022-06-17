93c17fdb59fbb463423d7fc18113d0706378f5da
Crouwing <mr.crouwing@gmail.com>
2022-06-17 15:18:16 +0300

    Упражнение по написанию кода 6: Задание на работу с функциями

--- JS/script_one.js
+++ JS/script_one.js
@@ -1,36 +1,36 @@
 "use strict";
 
-const usdCurr = 28;
-const uerCurr = 30;
-const discount = 0.9;
-
-function convert (amount, curr) {
-    return curr * amount;
+function sayHello(Name) {
+    return ("Привет" + "," + " " + Name + "!");
 }
 
-function promo (result) {
-    console.log(result * discount);
+console.log(sayHello("Антон"));
+
+// ------------------------------------------
+
+function returnNeighboringNumbers(number) {
+    return [number - 1, number, number + 1];
 }
 
-const res = convert(500, usdCurr);
-promo(res);
+console.log(returnNeighboringNumbers(5));
 
-// promo(convert(500, usdCurr)); - Альтернатива. Функция в функции.
+// ------------------------------------------
 
-function test () {
-    for (let i = 0; i < 5; i++) {
-        console.log(i);
-        if (i === 3) {
-            return;
+function getMathResult(NumberOne, NumberTwo) {
+    if (typeof(NumberTwo) != "number" || NumberTwo <= 0) {
+            return NumberOne;
+ }
+
+    let str = "";
+
+    for (let i = 1; i <= NumberTwo; i++) {
+        if (i === NumberTwo) {
+            str += NumberOne * i;
+        } else {
+            str += NumberOne * i + "---";
         }
-    }
-        console.log("done");
+    }   
+        return str;
 }
 
-test ();
-
-function doNothing () {}
-
-console.log(doNothing() === undefined);
-
-// Любая фукнция всегда возвращает что то, даже если не прирывать возвращает undefined )
+console.log(getMathResult (10, 14));
